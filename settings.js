af50a9f48c110ad4e4c14887c2da369eb5b7033f
cyberkavi0 <cyberkavi10@gmail.com>
2026-03-24 23:28:58 +0530

    beta

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~bsYQzQZa#w448DfIxIi19bzj_ueXXRnQXA9u2ojQWqcYa0PL8KHA"
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~PwgDECrS#eC-aIoAM4dW1vWYs9561O-XZEBLr-pYn7Rk9zWFNM5g"
 global.mongodburi = process.env.MONGODB_URI || "mongodb+srv://cyberkavi011_db_user:cyberkavi011_db_user@kavi-x-md-movie-bot-new.scruxlq.mongodb.net/0764891827-data?appName=kavi-x-md-movie-bot-new-v"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94766577249"
